269468574480018e558394b890cf6edbce7101fd
Brad Erickson <bderickson@gmail.com>
2019-11-15 18:23:29 -0800

    feat: Check block tags to erode leaves, wool, and mod-provided blocks

--- src/main/java/com/_13rac1/erosion/ErodableBlocks.java
+++ src/main/java/com/_13rac1/erosion/ErodableBlocks.java
@@ -10,12 +10,12 @@ import net.minecraft.block.Block;
 // leaves < sand < gravel < dirt < grass
 public class ErodableBlocks {
   public static final Integer MAX_RESISTANCE = 100;
-  private static final Integer LEAF_RESISTANCE = 1;
-  private static final Integer WOOL_RESISTANCE = 5;
+  private static final Integer LEAF_RESISTANCE = 5;
   private static final Integer SAND_RESISTANCE = 5;
+  private static final Integer WOOL_RESISTANCE = 10;
   private static final Integer GRAVEL_RESISTANCE = 20;
-  private static final Integer DIRT_RESISTANCE = 40;
-  private static final Integer GRASS_RESISTANCE = 60;
+  private static final Integer DIRT_RESISTANCE = 30;
+  private static final Integer GRASS_RESISTANCE = 40;
 
   // Warning: Blocks.SAND is null during startup, only run after start.
   private static HashMap<Block, Integer> blocks;
@@ -35,12 +35,24 @@ public class ErodableBlocks {
   }
 
   // Given a block, returns an Integer from 0->100 representing the erosion
-  // resistance of a block.
+  // resistance of the block.
   public static Integer getErosionResistance(Block block) {
     HashMap<Block, Integer> erodableBlocks = getList();
 
     if (erodableBlocks.containsKey(block)) {
       return erodableBlocks.get(block);
+    }
+
+    // Check block tags to erode leaves, wool, and mod-provided blocks.
+    // TODO: Less CPU to fill the hashmap with all values once.
+    if (BlockTags.LEAVES.contains(block)) {
+      return LEAF_RESISTANCE;
+    } else if (BlockTags.SAND.contains(block)) {
+      return SAND_RESISTANCE;
+    } else if (BlockTags.DIRT_LIKE.contains(block)) {
+      return DIRT_RESISTANCE;
+    } else if (BlockTags.WOOL.contains(block)) {
+      return WOOL_RESISTANCE;
     }
 
     return MAX_RESISTANCE;
