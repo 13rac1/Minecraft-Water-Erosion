55105b857a04fe40b5910c8279f3f98513503103
Brad Erickson <bderickson@gmail.com>
2020-03-14 23:07:19 -0700

    feat: forge-1.14.4: Add junit with basic test

---
+++ forge-1.14.4/src/test/java/com/_13rac1/erosion/common/HelloTest.java
@@ -0,0 +1,18 @@
+package com._13rac1.erosion.common;
+
+import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.AfterAll;
+// import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.BeforeAll;
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.params.ParameterizedTest;
+// import org.junit.jupiter.params.provider.Arguments;
+// import org.junit.jupiter.params.provider.MethodSource;
+
+public class HelloTest {
+  @Test
+  void test_test() {
+    Assertions.assertArrayEquals(new String[0], new String[0]);
+  }
+}
