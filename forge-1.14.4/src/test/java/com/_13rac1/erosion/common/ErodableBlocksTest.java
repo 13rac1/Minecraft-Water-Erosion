cfc12bb26a15b1630d63d349aca18bb7a71854a6
Brad Erickson <bderickson@gmail.com>
2020-03-21 23:17:22 -0700

    refactor: Move test FakeBlock to a separate class

--- forge-1.14.4/src/test/java/com/_13rac1/erosion/common/ErodableBlocksTest.java
+++ forge-1.14.4/src/test/java/com/_13rac1/erosion/common/ErodableBlocksTest.java
@@ -1,7 +1,5 @@
 package com._13rac1.erosion.common;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
@@ -12,62 +10,12 @@ import org.junit.jupiter.api.Test;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.material.Material;
 
 public class ErodableBlocksTest {
-  // src: https://stackoverflow.com/a/3301720
-  static void setFinalStatic(Field field, Object newValue) throws Exception {
-    field.setAccessible(true);
 
-    Field modifiersField = Field.class.getDeclaredField("modifiers");
-    modifiersField.setAccessible(true);
-    modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-
-    field.set(null, newValue);
-  }
-
-  // Create a fake block so the Block.class field is not null for that entry.
-  static void fakeBlock(String blockName, Material material) throws Exception {
-    Block.Properties properties = Block.Properties.create(material);
-    Block block = new Block(properties);
-
-    setFinalStatic(Blocks.class.getField(blockName), block); // Uppercase block name.
-  }
-
-  // Setup all blocks.NAME values in used in tests, because they are normally set
-  // during Minecraft startup and therefore are null values, which means using
-  // them in ErodableBlocks.getErodables() will cause a null exception.
   @BeforeAll
   static void beforeAll() throws Exception {
-    fakeBlock("AIR", Material.AIR);
-    fakeBlock("WATER", Material.WATER);
-
-    fakeBlock("CLAY", Material.CLAY);
-    fakeBlock("RED_SAND", Material.SAND);
-    fakeBlock("SAND", Material.SAND);
-    fakeBlock("GRAVEL", Material.MISCELLANEOUS);
-    fakeBlock("COARSE_DIRT", Material.MISCELLANEOUS);
-    fakeBlock("FARMLAND", Material.MISCELLANEOUS);
-    fakeBlock("DIRT", Material.MISCELLANEOUS);
-    fakeBlock("PODZOL", Material.MISCELLANEOUS);
-    fakeBlock("GRASS_BLOCK", Material.MISCELLANEOUS);
-    fakeBlock("GRASS", Material.MISCELLANEOUS);
-    fakeBlock("MOSSY_COBBLESTONE", Material.MISCELLANEOUS);
-    fakeBlock("MOSSY_COBBLESTONE_SLAB", Material.MISCELLANEOUS);
-    fakeBlock("MOSSY_COBBLESTONE_STAIRS", Material.MISCELLANEOUS);
-    fakeBlock("MOSSY_COBBLESTONE_WALL", Material.MISCELLANEOUS);
-    fakeBlock("COBBLESTONE", Material.MISCELLANEOUS);
-    fakeBlock("COBBLESTONE_SLAB", Material.MISCELLANEOUS);
-    fakeBlock("COBBLESTONE_STAIRS", Material.MISCELLANEOUS);
-    fakeBlock("COBBLESTONE_WALL", Material.MISCELLANEOUS);
-    fakeBlock("MOSSY_STONE_BRICKS", Material.MISCELLANEOUS);
-    fakeBlock("MOSSY_STONE_BRICK_SLAB", Material.MISCELLANEOUS);
-    fakeBlock("MOSSY_STONE_BRICK_STAIRS", Material.MISCELLANEOUS);
-    fakeBlock("MOSSY_STONE_BRICK_WALL", Material.MISCELLANEOUS);
-    fakeBlock("STONE_BRICKS", Material.MISCELLANEOUS);
-    fakeBlock("STONE_BRICK_SLAB", Material.MISCELLANEOUS);
-    fakeBlock("STONE_BRICK_STAIRS", Material.MISCELLANEOUS);
-    fakeBlock("STONE_BRICK_WALL", Material.MISCELLANEOUS);
+    FakeBlock.setupFakeBlocks();
   }
 
   @Test
