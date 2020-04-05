f284e06ee9f4a77795d62b2ff6e3531a3788b56d
Brad Erickson <bderickson@gmail.com>
2020-04-04 18:40:19 -0700

    fix: forge-1.12: Fix DupeMod crash and include all files in shadowJar
    FMLCorePluginContainsFMLMod was not set true in the Manifest, which causes
    the server (only) to crash with:
    
    Encountered an unexpected exception
    net.minecraftforge.fml.common.DuplicateModsFoundException:
    Duplicate Mods:
    	water-erosion : /data/./mods/water-erosion-1.12.2-forge-1.1.0-alpha1-full.jar
    	water-erosion : /data/./mods/water-erosion-1.12.2-forge-1.1.0-alpha1-full.jar
    
    Setting it true makes the release Jar work on both server and clients, but
    breaks the mod in development. Workaround is to set it in the Manifest during
    the shadowJar build.
    
    Now includes all needed files correctly during shadowJar build, specifically
    including the mixin refmap and embedded Spongepowered Mixin classes.
    
    Fixes #15
    https://github.com/13rac1/Minecraft-Water-Erosion/issues/15

--- forge-1.12.2/src/main/java/com/_13rac1/erosion/forge/WaterErosionMod.java
+++ forge-1.12.2/src/main/java/com/_13rac1/erosion/forge/WaterErosionMod.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.Logger;
 public class WaterErosionMod {
   public static final String MODID = "water-erosion";
   public static final String NAME = "Water Erosion";
-  public static final String VERSION = "1.1.0-alpha1";
+  public static final String VERSION = "1.1.0-alpha3";
 
   public static Logger logger;
 
