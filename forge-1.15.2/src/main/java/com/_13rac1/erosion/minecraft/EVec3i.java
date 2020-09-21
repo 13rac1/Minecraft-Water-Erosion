b7c65bfd48256fc65d02cd92d6e8f435851b7e25
Brad Erickson <bderickson@gmail.com>
2020-09-21 01:25:25 -0700

    refactor: Convert forge-1.15.2 to E* prefix wrapper classes

---
+++ forge-1.15.2/src/main/java/com/_13rac1/erosion/minecraft/EVec3i.java
@@ -0,0 +1,43 @@
+package com._13rac1.erosion.minecraft;
+
+import net.minecraft.util.math.Vec3i;
+
+public class EVec3i {
+  Vec3i vec;
+
+  public EVec3i() {
+    this.vec = new Vec3i(0, 0, 0);
+  }
+
+  public EVec3i(Vec3i vec) {
+    this.vec = vec;
+  }
+
+  public EVec3i(int x, int y, int z) {
+    this.vec = new Vec3i(x, y, z);
+  }
+
+  public EVec3i(double x, double y, double z) {
+    this.vec = new Vec3i(x, y, z);
+  }
+
+  private Vec3i getVec() {
+    return this.vec;
+  }
+
+  public int getX() {
+    return this.vec.getX();
+  }
+
+  public int getY() {
+    return this.vec.getY();
+  }
+
+  public int getZ() {
+    return this.vec.getZ();
+  }
+
+  public EVec3i crossProduct(EVec3i vec) {
+    return new EVec3i(this.vec.crossProduct(vec.getVec()));
+  };
+}
--- forge-1.15.2/src/main/java/net/minecraft/tag/BlockTags.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.tag;
-
-// The import is named net.minecraft.tag.BlockTags in Fabric, but
-// net.minecraft.tags.BlockTags in Forge, so this hack means the Forge build of
-// the common library will find the import.
-
-// TODO: Can/should the Fabric mappings be changed?
-public class BlockTags extends net.minecraft.tags.BlockTags {
-}
