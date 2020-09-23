13407b68ac25383dbace768bd509e33d15ec95fa
Brad Erickson <bderickson@gmail.com>
2020-09-22 21:48:22 -0700

    refactor: Convert fabric-1.14.4 to E* prefix common wrapper classes

---
+++ fabric-1.14.4/src/main/java/com/_13rac1/erosion/minecraft/EWorld.java
@@ -0,0 +1,43 @@
+package com._13rac1.erosion.minecraft;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FluidBlock;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.world.World;
+
+import com._13rac1.erosion.common.ErosionWorld;
+
+public class EWorld implements ErosionWorld {
+  private World world;
+
+  public EWorld(World world) {
+    this.world = world;
+  }
+
+  public BlockState getBlockState(EBlockPos pos) {
+    return this.world.getBlockState(pos.getPos());
+  }
+
+  public Block getBlock(EBlockPos pos) {
+    return this.world.getBlockState(pos.getPos()).getBlock();
+  }
+
+  public Boolean setBlockState(EBlockPos pos, BlockState newState, Integer flags) {
+    return this.world.setBlockState(pos.getPos(), newState, flags);
+  }
+
+  public int getSeaLevel() {
+    return this.world.getSeaLevel();
+  }
+
+  public EVec3d getFlowVelocity(BlockState state, EBlockPos pos) {
+    FluidState fluidState = state.getFluidState();
+    return new EVec3d(fluidState.getVelocity(this.world, pos.getPos()));
+  }
+
+  public Boolean isFluidBlock(Block block) {
+    return block instanceof FluidBlock;
+  }
+
+}
