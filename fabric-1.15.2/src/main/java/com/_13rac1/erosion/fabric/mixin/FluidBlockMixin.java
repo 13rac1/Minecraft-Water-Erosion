ee496626cecfc42ed1f603e875518db4729f5d8a
Brad Erickson <bderickson@gmail.com>
2020-09-22 22:17:44 -0700

    refactor: Convert fabric-1.15.2 to E* prefix common wrapper classes

--- fabric-1.15.2/src/main/java/com/_13rac1/erosion/fabric/mixin/FluidBlockMixin.java
+++ fabric-1.15.2/src/main/java/com/_13rac1/erosion/fabric/mixin/FluidBlockMixin.java
@@ -3,21 +3,20 @@ package com._13rac1.erosion.fabric.mixin;
 import java.util.Random;
 
 import net.minecraft.block.FluidBlock;
-import net.minecraft.fluid.FluidState;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.util.math.Vec3d;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com._13rac1.erosion.common.ErosionWorld;
 import com._13rac1.erosion.common.Tasks;
+import com._13rac1.erosion.minecraft.EBlockPos;
+import com._13rac1.erosion.minecraft.EWorld;
 
 @Mixin(FluidBlock.class)
 public class FluidBlockMixin extends Block {
@@ -25,39 +24,6 @@ public class FluidBlockMixin extends Block {
 
 	public FluidBlockMixin(Settings settings) {
 		super(settings);
-	}
-
-	class FabricWorld implements ErosionWorld {
-		private ServerWorld world;
-
-		public FabricWorld(ServerWorld world) {
-			this.world = world;
-		}
-
-		public BlockState getBlockState(BlockPos pos) {
-			return this.world.getBlockState(pos);
-		}
-
-		public Block getBlock(BlockPos pos) {
-			return this.world.getBlockState(pos).getBlock();
-		}
-
-		public Boolean setBlockState(BlockPos pos, BlockState newState, Integer flags) {
-			return this.world.setBlockState(pos, newState, flags);
-		}
-
-		public int getSeaLevel() {
-			return this.world.getSeaLevel();
-		}
-
-		public Vec3d getFlowVelocity(BlockState state, BlockPos pos) {
-			FluidState fluidState = state.getFluidState();
-			return fluidState.getVelocity(this.world, pos);
-		}
-
-		public Boolean isFluidBlock(Block block) {
-			return block instanceof FluidBlock;
-		}
 	}
 
 	@Override
@@ -68,8 +34,8 @@ public class FluidBlockMixin extends Block {
 
 	@Inject(method = "randomTick", at = @At("HEAD"), require = 1)
 	private void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random rand, CallbackInfo info) {
-		FabricWorld fabricWorld = new FabricWorld(world);
+		EWorld fabricWorld = new EWorld(world);
 
-		tasks.run(state, fabricWorld, pos, rand);
+		tasks.run(state, fabricWorld, new EBlockPos(pos), rand);
 	}
 }
